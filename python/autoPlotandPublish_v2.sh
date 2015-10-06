41948e7ebf9e422374e192f8e1285ba9e6ac6938
threus <threus@cern.ch>
2015-10-06 20:59:19 +0200

    added cosmics

--- python/autoPlotandPublish_v2.sh
+++ python/autoPlotandPublish_v2.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#ZeroBias STRIPS
-rm -rf ./JSON/*
-python ./trendPlots.py -C cfg/trendPlotsDQM_cronPPPromptStrips.ini -C cfg/trendPlotsStrip_General_2015.ini -C cfg/trendPlotsStrip_TEC_2015.ini -C cfg/trendPlotsStrip_TID_2015.ini -C cfg/trendPlotsStrip_TIB.ini -C cfg/trendPlotsStrip_TOB.ini -C cfg/trendPlotsStripG2.ini -C cfg/trendPlotsStrip_StoN.ini --dataset ZeroBias1 --epoch Run2015 -r "run > 246907" --reco Prompt
-cp ./JSON/* /data/users/event_display/HDQM/v2/HDQM_NUST/alljsons/2015/Prompt/ZeroBias/Strips/DECO/
-
 #StreamExpress STRIPS
 rm -rf ./JSON/*
 python ./trendPlots.py -C cfg/trendPlotsDQM_cronPPExpressStrips.ini -C cfg/trendPlotsStrip_General_2015.ini -C cfg/trendPlotsStrip_TEC_2015.ini -C cfg/trendPlotsStrip_TID_2015.ini -C cfg/trendPlotsStrip_TIB.ini -C cfg/trendPlotsStrip_TOB.ini -C cfg/trendPlotsStripG2.ini -C cfg/trendPlotsStrip_StoN.ini --dataset StreamExpress --epoch Run2015 -r "run > 246907" --reco Express
-cp ./JSON/* /data/users/event_display/HDQM/v2/HDQM_NUST/alljsons/2015/StreamExpress/Strips/DECO/
+cp ./JSON/* /data/users/event_display/HDQM/v2/alljsons/2015/StreamExpress/Strips/DECO/
+
+#StreamExpressCosmics STRIPS
+rm -rf ./JSON/*
+python ./trendPlots.py -C cfg/trendPlotsDQM_cronCExpress.ini -C cfg/trendPlotsStrip_TotalClusterMultiplicity.ini -C cfg/trendPlotsStrip_StoN.ini --dataset StreamExpressCosmics --epoch Run2015 -r "run > 247205" --reco Express
+cp ./JSON/* /data/users/event_display/HDQM/v2/alljsons/2015/StreamExpressCosmics/Strips/DECO/
+
+#ZeroBias STRIPS
+rm -rf ./JSON/*
+python ./trendPlots.py -C cfg/trendPlotsDQM_cronPPPromptStrips.ini -C cfg/trendPlotsStrip_General_2015.ini -C cfg/trendPlotsStrip_TEC_2015.ini -C cfg/trendPlotsStrip_TID_2015.ini -C cfg/trendPlotsStrip_TIB.ini -C cfg/trendPlotsStrip_TOB.ini -C cfg/trendPlotsStripG2.ini -C cfg/trendPlotsStrip_StoN.ini --dataset ZeroBias --epoch Run2015 -r "run > 246907" --reco Prompt
+cp ./JSON/* /data/users/event_display/HDQM/v2/alljsons/2015/Prompt/ZeroBias/Strips/DECO/
+
